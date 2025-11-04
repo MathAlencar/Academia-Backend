c5ff15725f2e802862416da16381c2ab1af334e6
luana.araujo <luana.barreto@socin.com.br>
2025-11-04 18:21:56 -0300

    feat: Criação da regra de planos do personal

--- src/Models/PlanosPersonal.js
+++ src/Models/PlanosPersonal.js
@@ -2,51 +2,46 @@ import Sequelize, { Model } from 'sequelize';
 
 export default class PlanosPersonal extends Model {
   static init(sequelize) {
-    super.init({
-      tipo_plano: {
-        type: Sequelize.STRING(20),
-        allowNull: false,
-        validate: {
-          notEmpty: {
-            msg: 'Campo tipo_plano não pode ficar vazio',
-          },
-          len: {
-            args: [3, 20],
-            msg: 'Campo tipo_plano deve ter entre 3 e 20 caracteres',
+    super.init(
+      {
+        personal_id: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+        },
+        tipo_plano: {
+          type: Sequelize.ENUM('Experimental', 'Avulsa', 'Mensal', 'Bimestral', 'Trimestral'),
+          allowNull: false,
+        },
+        valor: {
+          type: Sequelize.DECIMAL(10, 2),
+          allowNull: false,
+          validate: {
+            isDecimal: {
+              msg: 'O valor deve ser um número decimal válido.',
+            },
+            min: {
+              args: [0],
+              msg: 'O valor deve ser maior que zero.',
+            },
           },
         },
       },
-      valor: {
-        type: Sequelize.DECIMAL(10, 2),
-        allowNull: false,
-        validate: {
-          isDecimal: {
-            msg: 'Campo valor deve ser numérico',
+      {
+        sequelize,
+        tableName: 'planos_personal',
+        indexes: [
+          {
+            unique: true,
+            fields: ['personal_id', 'tipo_plano'], // garante 1 plano de cada tipo por personal
           },
-          min: {
-            args: [0],
-            msg: 'Campo valor deve ser maior ou igual a zero',
-          },
-        },
-      },
-    }, {
-      sequelize,
-      tableName: 'planos_personal',
-      indexes: [
-        {
-          unique: true,
-          fields: ['personal_id', 'tipo_plano'],
-        },
-      ],
-    });
+        ],
+      }
+    );
 
     return this;
   }
 
   static associate(models) {
-    this.belongsTo(models.Personal, {
-      foreignKey: 'personal_id',
-      as: 'personal',
-    });
+    this.belongsTo(models.Personal, { foreignKey: 'personal_id' });
   }
 }
