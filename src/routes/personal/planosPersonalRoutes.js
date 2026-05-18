71d86e70192327589aa9fc224988c8c8b787a583
luana.araujo <luana.barreto@socin.com.br>
2026-05-17 22:10:22 -0300

    feat: validação personal na criação de planos

--- src/routes/personal/planosPersonalRoutes.js
+++ src/routes/personal/planosPersonalRoutes.js
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import PlanosPersonalController from '../../Controllers/personal/planosPersonalControllers.js';
+import personalLoginRiquered from '../../middlewares/personalLoginRiquered.js';
 
 const router = Router();
 
-router.post('/', PlanosPersonalController.store);
+router.post('/', personalLoginRiquered, PlanosPersonalController.store);
 router.put('/:id', PlanosPersonalController.update);
 router.get('/:personal_id', PlanosPersonalController.index);
 router.delete('/:id', PlanosPersonalController.delete);
